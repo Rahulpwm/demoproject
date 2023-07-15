1db2e0e089ad8cbaf6fe41a429ce0eada8f5381c
Rahul <rahulrajbgp50@gmail.com>
2023-07-15 18:52:44 +0530

    code

---
+++ Java/Bhabha/rahul.java
@@ -0,0 +1,10 @@
+package Bhabha;
+
+public class rahul {
+    public static void main(String args[]){
+        int a=8;
+        int b=0;
+        int sum=a+b;
+        System.out.println(sum);
+    }
+}
